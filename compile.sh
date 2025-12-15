9a56f57117fa8dfb0cac2acc0e66710b39a56d7c
Steve Rock <steve.rock@marquee-magic.com>
2025-12-15 13:33:16 -0500

    updated compile.sh

--- compile.sh
+++ compile.sh
@@ -13,8 +13,13 @@ python3 -m nuitka --standalone --onefile \
     --enable-plugin=pyside6 \
     --include-module=shutil \
     --output-filename=create-icon-files.bin \
+    --jobs=$(nproc) \
     --output-dir=build \
     --remove-output \
     create_icon_files.py
+
+# Copy assets to build directory so the binary can find them when run locally
+echo "--- Copying assets to build directory ---"
+cp -r assets build/
 
 echo "Compilation complete. Binary is at build/create-icon-files.bin"
